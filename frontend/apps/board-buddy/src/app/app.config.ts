8e77cd2c9b43e67119c051240d11524eebc7600b
sambalmueslie <github@sambalmueslie.de>
2025-10-19 23:53:37 +0200

    finish initial setup of frontend app with tailwind and primeng

---
+++ frontend/apps/board-buddy/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {appRoutes} from './app.routes';
+import {providePrimeNG} from "primeng/config";
+import Aura from '@primeuix/themes/aura'
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(appRoutes),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ],
+};
---
+++ frontend/apps/board-buddy/src/app/app.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {Card} from "primeng/card";
+import {ButtonDirective} from "primeng/button";
+
+@Component({
+  imports: [RouterModule, Card, ButtonDirective],
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected title = 'board-buddy';
+
+  sayHello() {
+    alert('Hello from Board Buddy!');
+  }
+}
