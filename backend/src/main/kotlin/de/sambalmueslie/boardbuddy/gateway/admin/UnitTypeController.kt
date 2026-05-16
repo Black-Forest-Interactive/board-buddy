d56ac8fd4cd5e6ca6c90e58cf88815b71b6e80f2
sambalmueslie <github@sambalmueslie.de>
2026-05-16 18:34:16 +0200

    fix compile errors

--- backend/src/main/kotlin/de/sambalmueslie/boardbuddy/gateway/admin/UnitTypeController.kt
+++
@@ -1,9 +0,0 @@
-package de.sambalmueslie.boardbuddy.gateway.admin
-
-import io.micronaut.http.annotation.Controller
-import io.swagger.v3.oas.annotations.tags.Tag
-
-@Controller(value = "/api/portal/rule-set")
-@Tag(name = "Admin Rule Set API")
-class RuleSetController {
-}
