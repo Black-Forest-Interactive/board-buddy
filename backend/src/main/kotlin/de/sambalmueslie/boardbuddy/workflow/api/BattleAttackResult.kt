abbb45f027cab38ed2c37db39a27848c9bd0a30d
sambalmueslie <github@sambalmueslie.de>
2026-06-05 23:19:23 +0200

    add barbarians and ai

--- backend/src/main/kotlin/de/sambalmueslie/boardbuddy/workflow/api/BattleAttackResult.kt
+++
@@ -1,8 +0,0 @@
-package de.sambalmueslie.boardbuddy.workflow.api
-
-import de.sambalmueslie.boardbuddy.engine.api.CombatAction
-
-data class BattleAttackResult(
-    val workflow: Workflow,
-    val actions: List<CombatAction>
-)
